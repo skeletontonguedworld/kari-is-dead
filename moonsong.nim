5965306ef10b036a0f5f7c1c1a4c14a759eb43d8
skeletontonguedworld <129410383+skeletontonguedworld@users.noreply.github.com>
2023-07-23 16:46:40 +0200

    outlaw
    (powciągane rzeczy do pętelek, działa lepij, nadal są problemy)

--- moonsong.nim
+++ moonsong.nim
@@ -5,9 +5,6 @@ var pajunk1hp = 100
 var pajunk2hp = 125
 var pajunkgracza = 100
 
-echo "Witam w naszej pajunczej walce"
-echo "Nix tu nie będzie miało sensu, ale zobaczymy, cu z tego wyjdzie XD"
-
 proc wygrana (win: string = "WYGRAŁES MORDUCHNO"): string =
     echo $win
 
@@ -17,15 +14,17 @@ proc przegrana (lsr: string = "PRZEGRYWIE, PRZEGRAŁEŚ WALKĘ, NAWET JAK PRAWID
 proc wynik (wnk: string = "A OTO WYNIK NASZEJ WALKI"): string =
     while pajunkgracza > 0:
 
-        if pajunkgracza > pajunk1hp:
+        if pajunkgracza > pajunk1hp or pajunkgracza > pajunk2hp:
             echo wygrana()
             break
-        elif pajunkgracza < pajunk1hp:
+        elif pajunkgracza < pajunk1hp or pajunkgracza > pajunk2hp:
             echo przegrana()
             break
 
 while pajunkgracza > 0:
 
+    echo "Witam w naszej pajunczej walce"
+    echo "Nix tu nie będzie miało sensu, ale zobaczymy, cu z tego wyjdzie XD"
     echo "Zmierzysz się z pierwszym potężnym przeciwnikiem, pajunkiem, ktory od 10 lat podgryza zwłoki na cmentarzu"
     echo "Powitajmy Spiderellę"
     echo "klasku klasku"
@@ -38,17 +37,16 @@ while pajunkgracza > 0:
         pajunkgracza -= rand(1..100)
         pajunk1hp -= rand(1..100)
 
+        echo "Po ciężkiej walce Twoje zdrówko to " & $pajunkgracza & " a zdróweczko Twojego przeciwnika to " & $pajunk1hp
+        echo wynik()
+
         break
 
     else: 
         echo "To ja nie rozumiem, po co przyłazisz na walki pajuncze, jak walczyć nie kcesz, szok..."
-
         break
 
-echo "Po ciężkiej walce Twoje zdrówko to " & $pajunkgracza & " a zdróweczko Twojego przeciwnika to " & $pajunk1hp
-echo wynik()
-
-while pajunkgracza > 0:
+while pajunkgracza > pajunk1hp:
 
     echo "Twoim kolejnym przeciwnikiem jest pajunk który przeszedł wiele w swoim żyćku, wiele napisał, wiele alkoholu wypił, w wielu barowych bitwach brał udział"
     echo "Idzie do nas Spidery Pajunczaski"
@@ -61,6 +59,9 @@ while pajunkgracza > 0:
         pajunkgracza -= rand(1..100)
         pajunk2hp -= rand(1..125)
 
+        echo "Po kolejnej walce, która nie była łatwa i wywołała wiele bólu i emocji, Twoje zdrówko wynosi " & $pajunkgracza & " a zdrówko tego drugiego, którego powinniśmy zobaczyć " & $pajunk2hp
+        echo wynik()
+
         break
 
     else:
@@ -68,9 +69,6 @@ while pajunkgracza > 0:
 
         break
 
-echo "Po kolejnej walce, która nie była łatwa i wywołała wiele bólu i emocji, Twoje zdrówko wynosi " & $pajunkgracza & " a zdrówko tego drugiego, którego powinniśmy zobaczyć " & $pajunk2hp
-echo wynik()
-
 echo "Dobru, koniec walki na dziś XD" 
 
-# dopracuj jeszcze pętelki, żeby lepij działały, ale ogółem jest hehe git
+# nadal jest problem z echoskami w else!
